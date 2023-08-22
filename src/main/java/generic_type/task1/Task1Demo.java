56bec532d6ca7fed7920032edfd9afc4424e7fc1
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-08-22 21:35:21 +0200

    task 1 from generic type

---
+++ src/main/java/generic_type/task1/Task1Demo.java
@@ -0,0 +1,13 @@
+package generic_type.task1;
+
+public class Task1Demo {
+    public static void main(String[] args) {
+
+        Pair<String, Integer> pair1 = new Pair<>("key1", 1);
+
+        System.out.println(pair1);
+
+        Pair<Integer, Integer> pair2 = new Pair<>(2, 1);
+
+    }
+}
