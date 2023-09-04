3d01b8f7be444a91ee5b6f9369b9afbc33600cec
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-09-04 21:49:06 +0200

    task 1 from io two difficult solution

--- src/main/java/io/task1/Task1.java
+++ src/main/java/io/task1/Task1.java
@@ -15,7 +15,8 @@ public class Task1 {
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\macie\\IdeaProjects\\advanced_task_revision\\src\\main\\java\\io\\task1\\plik2.txt");
         File file2 = new File("C:\\Users\\macie\\IdeaProjects\\advanced_task_revision\\src\\main\\java\\io\\task1");
-
+        // file służy do "dobrania się" do jakiegoś folderu/ katalogu
+        
         file.createNewFile(); // konieczne, by utworzyć plik
         System.out.println(list("C:\\Users\\macie\\IdeaProjects\\advanced_task_revision\\src\\main\\java\\io\\task1"));
         System.out.println("**********");
