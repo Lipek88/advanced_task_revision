8e9d14a1c4f80d04cf298ea76d4207537a98527c
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-09-06 23:01:07 +0200

    task 3 from io

---
+++ src/main/java/io/task3/Task3.java
@@ -0,0 +1,32 @@
+package io.task3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Task3 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String strLine = "";
+        try {
+            String filename =
+                    "C:\\Users\\macie\\IdeaProjects\\advanced_task_revision\\src\\main\\java\\io\\task3\\plikTask3.txt";
+            FileWriter fw = new FileWriter(filename, true);
+            fw.write("Java I/0 Exercises1111111111111\n");
+            fw.close();
+            BufferedReader br = new BufferedReader(new
+                    FileReader(filename));
+            while (strLine != null) {
+//                stringBuilder.append(strLine);
+//                stringBuilder.append(System.lineSeparator());
+                strLine = br.readLine();
+                System.out.println(strLine);
+            }
+            br.close();
+        } catch (IOException ioe) {
+            System.err.println("I0Exception: " +
+                    ioe.getMessage());
+        }
+    }
+}
